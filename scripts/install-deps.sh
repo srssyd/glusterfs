c474258213e5af75d93a8a0b5207b42631f85235
Yingdi Shan <shanyd15@mails.tsinghua.edu.cn>
2016-11-18 22:09:26 +0800

    Add gitlab ci file and script

---
+++ scripts/install-deps.sh
@@ -0,0 +1,22 @@
+echo 'Begin to install epel.'
+
+yum -y install epel-release.noarch
+
+if [[ $? -ne 0 ]]; then
+	echo 'Installsion for epel do not succeed.'
+	exit $?	
+fi
+
+echo 'Begin to switch mirror.'
+cp ./epel.repo /etc/yum.repos.d/epel.repo
+echo 'Swith completed.'
+
+echo 'Begin to install dependency.'
+yum -y install automake autoconf libtool flex bison openssl-devel libxml2-devel python-devel libaio-devel libibverbs-devel librdmacm-devel readline-devel lvm2-devel glib2-devel userspace-rcu-devel libcmocka-devel libacl-devel sqlite-devel userspace-rcu-devel.x86_64
+
+if [[ $? -ne 0 ]]; then
+	echo 'Failed to install dependency.'
+	exit $?
+fi
+
+echo 'Dependency installed successfully'
